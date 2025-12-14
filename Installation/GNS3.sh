912b6b8937d39450e4ce94cd9c6111b59fe1ee0e
yemnox <emna.drihem@insat.ucar.tn>
2025-12-14 23:35:48 +0100

    GNS3 Documentation link

--- Installation/GNS3.sh
+++ Installation/GNS3.sh
@@ -1,3 +1,4 @@
+#visit https://docs.gns3.com/docs/getting-started/installation/linux/ for more details
 sudo add-apt-repository ppa:gns3/ppa
 sudo apt update                                
 sudo apt install gns3-gui gns3-server
